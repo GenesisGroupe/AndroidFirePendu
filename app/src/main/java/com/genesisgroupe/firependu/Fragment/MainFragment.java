e3e74c38e3918e5b6624970ade9dc5582f2ac5f7
Alexandre Bailly <alexandre.bailly25@gmail.om>
2016-11-21 16:52:36 +0100

    begin fragment

--- app/src/main/java/com/genesisgroupe/firependu/Fragment/MainFragment.java
+++ app/src/main/java/com/genesisgroupe/firependu/Fragment/MainFragment.java
@@ -1,7 +1,22 @@
 package com.genesisgroupe.firependu.Fragment;
 
-/**
- * Created by alex on 21/11/2016.
- */
-public class MainFragment {
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class MainFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return  inflater.inflate(R.layout.grament_list,false);
+    }
+
+
+
 }
